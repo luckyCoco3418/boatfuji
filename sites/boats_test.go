e200b0450f3f59c552ba3931a9186bb4a46b5fb6
coco50511 <coco50511@outlook.com>
2020-09-15 15:31:53 +0800

    boats.go boats_test.go first added

---
+++ sites/boats_test.go
@@ -0,0 +1,28 @@
+package sites
+
+import (
+	"log"
+	"testing"
+
+	"../api"
+)
+
+func TestBoats(t *testing.T) {
+	// site := &Boatsetter{StoreData: true, WriteSQL: false}
+	site := &Boats{StoreData: false, WriteSQL: false}
+	if site.StoreData {
+		api.Start()
+	}
+	if site.WriteSQL {
+		startSQL()
+	}
+	err := site.Harvest("")
+	// err := site.Harvest("https://www.boats.com/")
+	// err := site.Harvest("https://www.boats.com/boats/ocean-alexander/45-divergence-coupe-7586270/")
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	if site.WriteSQL {
+		finishSQL()
+	}
+}
